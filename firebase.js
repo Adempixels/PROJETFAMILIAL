c38d2d9fbcd0b8be8ace6fd136cc3305519bec29
Adempixels <ademsahraoui2503@gmail.com>
2026-03-15 10:01:49 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,8 +1,6 @@
-// Import Firebase
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
-// Configuration Firebase
 const firebaseConfig = {
 apiKey: "AIzaSyDLkooDKMD-rItkYe3J6ichWRCG6mn1KF4",
 authDomain: "projet-ecole-familial.firebaseapp.com",
@@ -12,11 +10,7 @@ messagingSenderId: "349833342586",
 appId: "1:349833342586:web:9b84d2ac12eb0f04fcd5c3"
 };
 
-// Initialiser Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialiser Firestore
 const db = getFirestore(app);
 
-// Exporter la base
 export { db };
